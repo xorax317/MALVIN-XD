8235e4e1cdc230cee75d6706506eb93a16024302
xorax317 <botslash34@gmail.com>
2025-08-17 14:52:54 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -2,7 +2,7 @@
 module.exports = {
   SESSION_ID: "",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "233593621358", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
